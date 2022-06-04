b5633a7f9ff010da1524e8bfb23324056e04754d
J5 <jheyer@opentext.com>
2022-06-04 13:21:45 -0700

    module updates

--- aws_nat_gateway/main.tf
+++ aws_nat_gateway/main.tf
@@ -2,4 +2,7 @@ resource "aws_nat_gateway" "default" {
   allocation_id     = var.eip_allocation_id
   subnet_id         = var.subnet_id
   connectivity_type = var.eip_allocation_id == null ? "private" : "public"
+  tags = {
+    Name = var.name
+  }
 }
--- aws_nat_gateway/variables.tf
+++ aws_nat_gateway/variables.tf
@@ -5,3 +5,7 @@ variable "eip_allocation_id" {
 variable "subnet_id" {
   type = string
 }
+variable "name" {
+  type    = string
+  default = null
+}
